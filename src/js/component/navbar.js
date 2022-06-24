8ef025ab462f6a83341ee3f4f6bbe01490d315c2
anabartos <ana.l.bartos@gmail.com>
2022-06-24 17:26:14 +0000

    Se añade componente Navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-light bg-light px-5">
+		  <div className="container-fluid">
+			<a class="navbar-brand" href="#">
+			  <img
+				src="https://www.pngplay.com/wp-content/uploads/2/Star-Wars-Logo-Transparent-Images.png"
+				alt=""
+				width="80"
+				height="auto"
+				class="d-inline-block align-text-top"
+			  />
+			</a>
+			<div class="btn-group">
+			  <button
+				type="button"
+				class="btn btn-primary dropdown-toggle"
+				data-bs-toggle="dropdown"
+				aria-expanded="false"
+			  >
+				Favorites
+			  </button>
+			  <ul class="dropdown-menu">
+				<li>
+				  <a class="dropdown-item" href="#"></a>
+				</li>
+			  </ul>
 			</div>
+		  </div>
 		</nav>
+	  
 	);
 };
